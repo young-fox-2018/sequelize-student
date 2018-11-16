47b2c25bfad5bf5f2ca75eba75c2bfdebed0ff06
arnoldherod <arnold.herod@gmail.com>
2018-11-16 18:27:00 +0700

    initial submission

---
+++ Controllers/studentController.js
@@ -0,0 +1,63 @@
+const View = require('../Views/View')
+const Model = require('../models')
+const Student = Model.Student
+
+class studentController{
+
+    static execute(cmd, input){
+        switch (cmd) {
+
+            case "add":
+                Student.create({
+                    first_name: input[0],
+                    last_name: input[1],
+                    gender: input[2],
+                    birthday: input[3],
+                    email: input[4],
+                    phone: input[5],
+                    "tinggi badan": input[6]
+                })
+                 .then(() => {
+                     View.display(`Data ${input[0]} ${input[1]} has been succesfully added to Students`)
+                 })
+                 .catch(err =>{
+                     console.log('masuk kesini')
+                    //  View.displayError(err)
+                 })
+                break;
+                
+            case "getFullName":
+                Student.findAll()
+                 .then(Students => {
+                     let fullNameAll = Students.map(Student => Student.getFullName())
+                     View.display(fullNameAll)
+                 })
+                break;
+                 
+            case "getAge":
+                Student.findAll()
+                 .then(Students => {
+                     let allAge = Students.map(Student => Student.getAge())
+                     View.display(allAge)
+                 })
+                break;
+
+            case "getFemaleStudents":
+                 Student.getFemaleStudents()
+                  .then(data => {
+                      data.map(females => {
+                          View.display(females)
+                      })
+                  })
+                  .catch(err => {
+                      View.displayError(err)
+                  })
+                break;
+            
+            default: View.help()
+                break;
+        }
+    }
+}
+
+module.exports = studentController
